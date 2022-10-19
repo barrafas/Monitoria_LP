2390cf94850fe11273ebdcf8f58d44ad6c7961d7
Lavínia <L4viniadias510@gmail.com>
2022-10-18 22:12:19 -0300

    .

--- Listas Monitoria/(15-10) Unit Test/numbers_phone.py
+++ Listas Monitoria/(15-10) Unit Test/numbers_phone.py
@@ -1,14 +1,17 @@
 import re
 def number_phone_br(number_phone):
-    if type(number_phone) == int:
-        number_phone = str(number_phone)
+    number_phone = str(number_phone)
     number_phone =  re.sub("[^0-9]+","", number_phone)
     if len(number_phone)<9:
         raise TypeError
-    if len(number_phone)== 9  :
+        
+    if len(number_phone)== 9  or len(number_phone)== 8:
         number_phone = "21"+number_phone
-    if len(number_phone)>9:
+
+    if number_phone[-11:-9] == "21":
         number_phone =  number_phone[-11:]
+    if number_phone[-10:-8] == "21":
+        number_phone =  number_phone[-10:]
     
     number_phone = int(number_phone)
     return number_phone
